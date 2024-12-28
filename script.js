9e3bfb4a63fed62ebc0e0f508ef4bff1d464cacb
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-12-28 12:01:29 +0530

    task cards

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-function dragstartHandler(ev) {
-    ev.dataTransfer.setData("text/plain", ev.target.id);
+// Add Task Card
+function addTaskCard() {
+    const taskCont = document.getElementById("todo-cont");
+    console.log(taskCont);
+    const card = `
+    <div id="task" class="task" draggable="true" ondragstart="dragstartHandler(event)"">
+        <strong>Title</strong>
+        <p>Description</p>
+    </div>
+    `
+    taskCont.innerHTML += card;
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-    // Get the element by id
-    const element = document.getElementById("task");
-    // Add the ondragstart event listener
-    element.addEventListener("dragstart", dragstartHandler);
-  });
+// Drag and Drop Handlers
+function dragstartHandler(ev) {
+    console.log("ev target");
+    console.log(ev.target);
+    ev.dataTransfer.setData("text", ev.target.id);
+    console.log("drag start called");
+}
+
+// window.addEventListener("DOMContentLoaded", () => {
+//     // Get the element by id
+//     const element = document.getElementsByClassName("tasks");
+//     // Add the ondragstart event listener
+//     element.addEventListener("dragstart", dragstartHandler);
+//   });
 
 function dragoverHandler(ev) {
     ev.preventDefault();
     ev.dataTransfer.dropEffect = "move";
+    console.log("drag over called");
 }
 
 function dropHandler(ev) {
+    console.log("drop called");
     ev.preventDefault();
     // Get the id of the target and add the moved element to the target's DOM
     const data = ev.dataTransfer.getData("text");
+    console.log(data);
     ev.target.appendChild(document.getElementById(data));
 }
